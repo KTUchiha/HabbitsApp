ebae15b866c04a2d5533fcda2efa55f05423e085
Kaavya Tatavarty <kaavya.tatavarty@gmail.com>
2022-04-10 07:47:14 -0400

    data classes

---
+++ HabitMaster/DaysModel+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  DaysModel+CoreDataClass.swift
+//  HabitMaster
+//
+//  Created by Roohi Tatavarty on 11/14/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(DaysModel)
+public class DaysModel: NSManagedObject {
+
+}
---
+++ HabitMaster/GoalModel+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  GoalModel+CoreDataClass.swift
+//  HabitMaster
+//
+//  Created by Roohi Tatavarty on 11/14/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(GoalModel)
+public class GoalModel: NSManagedObject {
+
+}
